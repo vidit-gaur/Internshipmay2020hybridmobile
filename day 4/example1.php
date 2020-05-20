b3aef72cebc3c89d7f89aad4268622f6a1657537
vidit <viditgaur1209@gmail.com>
2020-05-20 23:05:55 +0530

    day 4

---
+++ day 4/example1.php
@@ -0,0 +1,5 @@
+<h1>List of heading types</h1>
+<?php
+for($i=1;$i<=6;$i++)
+   echo "<h$i>this is heading $i</h$i>";
+?>
---
+++ day 4/example2.php
@@ -0,0 +1,7 @@
+<?php
+$n=5;
+$f=1;
+for($i=1;$i<=$n;$i++)
+ $f=$f*$i;
+ echo "factorial of $n is $f";
+ ?>
